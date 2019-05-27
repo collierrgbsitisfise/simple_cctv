ff77cfe0da226714821d4b377ed6663e83bab5d2
collierrgbsitisfise <vadim.vicolaev.v@gmail.com>
2019-05-27 22:55:08 +0300

    simple motion recognition

--- app.js
+++ app.js
@@ -1,15 +1,14 @@
-var cv = require("opencv4nodejs");
-var sleep = require("sleep");
+const cv = require("opencv");
+const sleep = require("sleep");
 
-var camera = new cv.VideoCapture(0); //open camera
+const camera = new cv.VideoCapture(0); //open camera
 
 //set the video size to 512x288
 camera.setWidth(512);
 camera.setHeight(288);
-var window = new cv.NamedWindow("Camera");
-var firstFrame, frameDelta, gray, thresh;
 
-sleep.sleep(3);
+let firstFrame, wasMotionDetected;
+
 camera.read(function(err, frame) {
   firstFrame = frame;
   //convert to grayscale
@@ -17,42 +16,50 @@ camera.read(function(err, frame) {
   firstFrame.gaussianBlur([21, 21]);
 });
 
+const updateFiertImage = () => {
+  return new Promise((res, rej) => {
+    camera.read(function(err, frame) {
+      frame.save("original.jpg");
+      firstFrame = frame;
+      firstFrame.cvtColor("CV_BGR2GRAY");
+      firstFrame.gaussianBlur([21, 21]);
+      res(true);
+    });
+  });
+};
+
+sleep.sleep(2);
+
 interval = setInterval(function() {
-  camera.read(function(err, frame) {
-    gray = frame.copy();
+  camera.read(async function(err, frame) {
+    wasMotionDetected = false;
+    let gray = frame.copy();
+
     gray.cvtColor("CV_BGR2GRAY");
     gray.gaussianBlur([21, 21]);
 
-    frameDelta = new cv.Matrix();
-    //compute difference between first frame and current frame
+    let frameDelta = new cv.Matrix();
+
     frameDelta.absDiff(firstFrame, gray);
-    thresh = frameDelta.threshold(25, 255);
+
+    let thresh = frameDelta.threshold(25, 255);
+
     thresh.dilate(2);
 
-    var cnts = thresh.findContours();
+    let cnts = thresh.findContours();
 
+    console.log(cnts.size());
     for (i = 0; i < cnts.size(); i++) {
       if (cnts.area(i) < 500) {
         continue;
       }
 
-      frame.putText(
-        "Motion Detected",
-        10,
-        20,
-        cv.FONT_HERSHEY_SIMPLEX,
-        [0, 0, 255],
-        0.75,
-        2
-      );
+      wasMotionDetected = true;
     }
 
-    window.show(frame);
-    keyPressed = window.blockingWaitKey(0, 50);
-
-    if (keyPressed == 27) {
-      //exit if ESC is pressed
-      clearInterval(interval);
+    if (wasMotionDetected) {
+      console.log("MOTION DETECTED!!");
+      await updateFiertImage();
     }
   });
-}, 20);
+}, 500);
